8f13e6e3d274dab40e78e86e3f0ba16774001ef2
CABRALBRUNO000 <55759357+CABRALBRUNO000@users.noreply.github.com>
2019-12-11 22:10:36 -0300

    auterações no projeto
    mudanças realizadas no tamanho dos objetos e no ambiente react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Login from './Login'
 
 function App() {
-  return (
-    <div>
-      <Login/>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        Login / >
+        <
+        /div>
+    );
 }
 
 export default App;
